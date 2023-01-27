58bfa8559bc4480da217fcaeed6f1e6e34d8750f
Sergio del Amo <sergio.delamo@softamo.com>
2023-01-27 13:32:55 +0100

    clean branch

--- validation/src/test/groovy/io/micronaut/validation/validator/map/Book.java
+++
@@ -1,9 +0,0 @@
-package io.micronaut.validation.validator.map;
-
-import io.micronaut.core.annotation.Introspected;
-import javax.validation.constraints.NotBlank;
-
-@Introspected
-record Book(
-    @NotBlank String title
-){}
