9982726990cae755252d8e227e6636fcb9607530
Denis Stepanov <denis.s.stepanov@oracle.com>
2023-05-16 09:31:50 -0500

    Add KSP interface test

---
+++ test-suite-kotlin/src/test/kotlin/io/micronaut/docs/ioc/validation/itfce/VehicleSpec.kt
@@ -0,0 +1,45 @@
+package io.micronaut.docs.ioc.validation.itfce
+
+import io.kotest.assertions.throwables.shouldThrow
+import io.micronaut.context.ApplicationContext
+import io.micronaut.context.exceptions.BeanInstantiationException
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+
+class VehicleSpec {
+
+    @Test
+    fun testValid() {
+        // tag::start[]
+        val map = mapOf(
+                "my.engine.cylinders" to "8",
+                "my.engine.crank-shaft.rod-length" to "7.0"
+        )
+        val applicationContext = ApplicationContext.run(map)
+
+        val vehicle = applicationContext.getBean(Vehicle::class.java)
+        // end::start[]
+
+        assertEquals("Ford Engine Starting V8 [rodLength=7.0]", vehicle.start())
+
+        applicationContext.close()
+    }
+
+    @Test
+    fun testInvalid() {
+        // tag::start[]
+        val map = mapOf(
+                "my.engine.cylinders" to "-10",
+                "my.engine.crank-shaft.rod-length" to "7.0"
+        )
+        val applicationContext = ApplicationContext.run(map)
+        val exception = shouldThrow<BeanInstantiationException> {
+            applicationContext.getBean(Vehicle::class.java)
+        }
+        assertTrue(exception.message!!.contains("EngineConfig.getCylinders - must be greater than or equal to 1"))
+
+        applicationContext.close()
+    }
+
+}
