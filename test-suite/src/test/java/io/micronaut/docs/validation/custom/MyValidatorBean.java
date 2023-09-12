3cee60e6d0ebb33916d7a1dfe4083d7ef2e8588b
j1m-renwick <52238993+j1m-renwick@users.noreply.github.com>
2023-09-12 10:20:26 +0100

    doc: Updated documentation to demonstrate custom validation bean setup (#198)
    * Updated documentation to demonstrate custom validation bean setup
    
    * Update MyValidatorBean.kt
    
    ---------
    
    Co-authored-by: Sergio del Amo <sergio.delamo@softamo.com>

---
+++ test-suite/src/test/java/io/micronaut/docs/validation/custom/MyValidatorBean.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2017-2020 original authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.micronaut.docs.validation.custom;
+
+// tag::imports[]
+
+import io.micronaut.core.annotation.AnnotationValue;
+import io.micronaut.core.annotation.Introspected;
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.core.annotation.Nullable;
+import io.micronaut.validation.validator.constraints.ConstraintValidator;
+import io.micronaut.validation.validator.constraints.ConstraintValidatorContext;
+import jakarta.inject.Singleton;
+// end::imports[]
+
+// tag::class[]
+@Singleton
+@Introspected
+public class MyValidatorBean implements ConstraintValidator<DurationPattern, Object> {
+
+    @Override
+    public boolean isValid(@Nullable Object value,
+                           @NonNull AnnotationValue<DurationPattern> annotationMetadata,
+                           @NonNull ConstraintValidatorContext context) {
+        context.messageTemplate("invalid duration ({validatedValue}), additional custom message"); // (1)
+        return value == null || value.toString().matches("^PT?[\\d]+[SMHD]{1}$");
+    }
+}
+// end::class[]
