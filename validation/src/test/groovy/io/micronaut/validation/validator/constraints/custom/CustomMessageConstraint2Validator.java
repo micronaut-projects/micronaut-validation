169e0da329879f06cdbd0006141043995db0e81c
Jeremy Grelle <grellej@unityfoundation.io>
2024-05-30 04:02:21 -0400

    Remove Introspected from MyValidatorBean example (#336)
    * Remove Introspected from MyValidatorBean example
    
    The docs are updated to no longer show the use of the Introspected
    annotation on the MyValidatorBean example, as it cannot successfully
    be used on a validator that is a managed bean with dependencies.
    
    DefaultInternalConstraintValidatorFactory is updated to include the
    exception cause in the stack trace when a validator cannot be
    initialized.
    
    The CustomMessageConstraint2Validator test class for
    CustomConstraintsSpec is updated to include a dependency that must
    successfully be injected in order to verify that this works as long as
    the validator is not also Introspected.
    
    * Remove unused import
    
    Co-authored-by: Tim Yates <tim.yates@gmail.com>
    
    * Remove unused import
    
    * Add test for expected failure case
    
    ---------
    
    Co-authored-by: Tim Yates <tim.yates@gmail.com>

--- validation/src/test/groovy/io/micronaut/validation/validator/constraints/custom/CustomMessageConstraint2Validator.java
+++ validation/src/test/groovy/io/micronaut/validation/validator/constraints/custom/CustomMessageConstraint2Validator.java
@@ -6,8 +6,16 @@ import jakarta.validation.ConstraintValidatorContext;
 
 @Singleton
 public class CustomMessageConstraint2Validator implements ConstraintValidator<CustomMessageConstraint, Object> {
+
+    private final CustomValidatorDependency otherBean;
+
+    public CustomMessageConstraint2Validator(CustomValidatorDependency otherBean) {
+        this.otherBean = otherBean;
+    }
+
     @Override
     public boolean isValid(Object value, ConstraintValidatorContext context) {
+        otherBean.doSomeStuff();
         context.buildConstraintViolationWithTemplate("custom invalid").addConstraintViolation();
         context.disableDefaultConstraintViolation();
         return false;
---
+++ validation/src/test/groovy/io/micronaut/validation/validator/constraints/custom/CustomValidatorDependency.java
@@ -0,0 +1,11 @@
+package io.micronaut.validation.validator.constraints.custom;
+
+import jakarta.inject.Singleton;
+
+@Singleton
+public class CustomValidatorDependency {
+
+    public void doSomeStuff() {
+
+    }
+}
